9a3e13562a28466403f8c68cc0b386cb6841c046
Zhenyu,Liu <liuzhenyulive@live.com>
2021-04-18 20:46:30 +0800

    Initialize the project

---
+++ DotNetCoreOnK8S/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreOnK8S.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
