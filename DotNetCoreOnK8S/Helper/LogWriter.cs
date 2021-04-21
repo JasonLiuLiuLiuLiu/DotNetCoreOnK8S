94af28c999bd14866ad13864f644dd68d2d79f10
Zhenyu,Liu <liuzhenyulive@live.com>
2021-04-22 00:10:25 +0800

    fix path issue

--- DotNetCoreOnK8S/Helper/LogWriter.cs
+++ DotNetCoreOnK8S/Helper/LogWriter.cs
@@ -9,18 +9,18 @@ namespace DotNetCoreOnK8S.Helper
 
         public static void Write(string logMessage)
         {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log";
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "log");
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
-            using StreamWriter w = File.AppendText(path + "\\" + "log.txt");
+            using StreamWriter w = File.AppendText(Path.Combine(path, "log.txt"));
             Log(logMessage, w);
         }
 
         private static void Log(string logMessage, TextWriter txtWriter)
         {
-            txtWriter.WriteLine("{0} {1}:{2}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString(), logMessage);
+            txtWriter.WriteLine("{0} {1} on Pod {2} :{3}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString(), Environment.GetEnvironmentVariable("POD_NAME"), logMessage);
         }
     }
 }
